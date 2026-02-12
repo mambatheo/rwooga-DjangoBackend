e8840678a09dab85fb201edc36e9424677742d93
mambatheo <muthegene61@gmail.com>
2026-02-12 21:47:58 +0200

    FIles added

--- rwoogaBackend/settings.py
+++ rwoogaBackend/settings.py
@@ -12,7 +12,8 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+#DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -151,7 +152,7 @@ TIKTOK_ICON_URL = config('TIKTOK_ICON_URL', default='')
 
 STORAGES = {   
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
@@ -178,13 +179,12 @@ VERIFICATION_CODE_EXPIRY_MINUTES = 10
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
-
 # Site Configuration
 SITE_URL = config('SITE_URL', default='http://localhost:3000')
 COMPANY_NAME = config('COMPANY_NAME', default='Rwooga')
